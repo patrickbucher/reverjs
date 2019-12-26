70e9a84cef3196a82396fe2f0959fc3ecd655d33
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-12-26 13:01:18 +0100

    determine opponent of given player and fields with a given state

--- src/board.js
+++ src/board.js
@@ -29,6 +29,47 @@ class Board {
         }
         this.fields = fields;
     }
+
+    static of(fields) {
+        let board = new Board();
+        if (fields.length != dimension) {
+            throw new RangeError(`fields requires ${dimension} rows`);
+        }
+        for (let r = 0; r < dimension; r++) {
+            let row = fields[r];
+            if (row.length != dimension) {
+                throw new RangeError(`row requires ${dimension} cols`);
+            }
+            for (let c = 0; c < dimension; c++) {
+                if (row[c] != empty && row[c] != black && row[c] != white) {
+                    throw new RangeError(`illegal value ${row[c]} ([${r}/${c}])`);
+                }
+            }
+        }
+        board.fields = fields;
+        return board;
+    }
+
+    getFieldsWithState(state) {
+        const fields = [];
+        for (const row in this.fields) {
+            for (const col in this.fields[row]) {
+                if (this.fields[row][col] == state) {
+                    fields.push([Number(row), Number(col)]);
+                }
+            }
+        }
+        return fields;
+    }
+
+    opponent(player) {
+        if (player == black) {
+            return white;
+        } else if (player == white) {
+            return black;
+        }
+        throw RangeError(`illegal player ${player}`);
+    }
 }
 
 module.exports = Board;
--- src/board.test.js
+++ src/board.test.js
@@ -2,7 +2,7 @@
 
 const Board = require('./board.js');
 
-const emptyBoard = [
+const initialBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,7 +13,67 @@ const emptyBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-it('create empty board', () => {
+const boardAfterTwoMoves = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 1, 1, 2, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+it('create initial board', () => {
     const board = new Board();
-    expect(board.fields).toEqual(emptyBoard);
+    expect(board.fields).toEqual(initialBoard);
+});
+
+it('find black fields on initial board', () => {
+    const board = new Board();
+    expect(board.getFieldsWithState(1)).toEqual([[3, 4], [4, 3]]);
+});
+
+it('find white fields on initial board', () => {
+    const board = new Board();
+    expect(board.getFieldsWithState(2)).toEqual([[3, 3], [4, 4]]);
+});
+
+it('find black fields after two moves', () => {
+    const board = Board.of(boardAfterTwoMoves);
+    expect(board.getFieldsWithState(1)).toEqual([
+        [3, 2], [3, 3], [4, 3]
+    ]);
+});
+
+it('find white fields after two moves', () => {
+    const board = Board.of(boardAfterTwoMoves);
+    expect(board.getFieldsWithState(2)).toEqual([
+        [2, 4], [3, 4], [4, 4]
+    ]);
+});
+
+it('find opponent of black player', () => {
+    const board = new Board();
+    const blackPlayer = 1;
+    const whitePlayer = 2;
+    const opponent = board.opponent(blackPlayer);
+    expect(opponent).toEqual(whitePlayer);
+});
+
+it('find opponent of black player', () => {
+    const board = new Board();
+    const blackPlayer = 1;
+    const whitePlayer = 2;
+    const opponent = board.opponent(whitePlayer);
+    expect(opponent).toEqual(blackPlayer);
+});
+
+it('find opponent of illegal player', () => {
+    const board = new Board();
+    const illegalPlayer = 3;
+    const exception = new RangeError(`illegal player ${illegalPlayer}`);
+    expect(() => {
+        board.opponent(illegalPlayer)
+    }).toThrow(exception);
 });
